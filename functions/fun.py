80f7e8d630cd69c2eacba06aaab29bcbebac6f5a
bittuanvesh <bittuanvesh007@gmail.com>
2025-06-26 18:27:32 +0530

    fun to dev1

---
+++ functions/fun.py
@@ -0,0 +1,53 @@
+
+# def anvesh():
+#     print(f"hi anvesh")
+# anvesh()
+
+
+
+# def details(name,*address):
+#     print(f"my name is {name}")
+#     print(f"my address is {address}")
+#     print(f"my pincode is {address[1]}")
+
+# details('anvesh','guntur',222
+
+
+
+
+# class details:
+#     def __init__(self,name,address,pincode):
+#         self.name=name
+#         self.address=address
+#         self.pincode=pincode
+    
+#     def details_1(self):
+#         print(f"my name is {self.name}")
+#         print(f"my address is {self.address}")
+#         print(f"my name is {self.pincode}")
+
+# ok=details('anvesh','bittu','chinna')
+# ok.details_1()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
